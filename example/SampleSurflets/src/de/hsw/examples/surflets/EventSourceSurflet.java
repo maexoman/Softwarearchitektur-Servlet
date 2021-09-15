707336cf8fc8aa73e7a28d32bac5bb532dffac03
maexomen <mex.heinrich@gmail.com>
2021-09-15 12:53:15 +0200

    Hier ein kleines Beispiel-Projekt

---
+++ example/SampleSurflets/src/de/hsw/examples/surflets/EventSourceSurflet.java
@@ -0,0 +1,24 @@
+package de.hsw.examples.surflets;
+
+import de.hsw.HttpRequest;
+import de.hsw.HttpResponse;
+import de.hsw.Surflet;
+
+public class EventSourceSurflet implements Surflet {
+
+    @Override
+    public void handleRequest(HttpRequest request, HttpResponse response) throws Exception {
+        response.status(200);
+        response.addHeader ("Content-Type", "text/event-stream");
+        response.addHeader("Cache-Control", "no-cache");
+        response.addHeader("Content-Length", "chunked");
+        response.sendStatusLineAndHeaders ();
+
+        for (int counter = 1; counter <= 100; counter += 1) {
+            response.send("data: {\"counter\":" + Integer.toString(counter) + "}\n\n");
+            Thread.sleep(1000);
+        }
+        return;
+    }
+
+}
---
+++ example/SampleSurflets/src/de/hsw/examples/surflets/IndexSurflet.java
@@ -0,0 +1,36 @@
+package de.hsw.examples.surflets;
+
+
+import de.hsw.HttpRequest;
+import de.hsw.HttpRequestBody;
+import de.hsw.HttpResponse;
+import de.hsw.Surflet;
+
+public class IndexSurflet implements Surflet {
+    @Override
+    public void handleRequest(HttpRequest request, HttpResponse response) throws Exception {
+        String name = request.getParameter ("name", "John Doe");
+
+        response.setContentType(HttpRequestBody.ContentType.TEXT_HTML);
+
+        response.append("<html>\n");
+
+        response.append("<head>");
+        response.append("</head>\n");
+
+        response.append("<body>\n");
+        response.append("<form action=\"/index.html\" method=\"get\">\n");
+
+        response.append("<label for=\"fname\">First name:</label>\n");
+        response.append("<input type=\"text\" id=\"fname\" name=\"name\"><br><br>\n");
+        response.append("<input type=\"submit\" value=\"Submit\">\n");
+
+        response.append("</form>\n");
+        response.append("<h1>Hallo " + name + "</h1>\n");
+        response.append("</body>\n");
+
+
+        response.append("</html>");
+        response.send();
+    }
+}
